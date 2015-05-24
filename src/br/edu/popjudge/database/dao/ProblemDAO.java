0c7c54cd4d4786035d3a5eba376b9496ff3676ce
José Renan <joserenansl99@gmail.com>
2015-05-23 22:20:02 -0300

    Ranking em manutenção

--- src/br/edu/popjudge/database/dao/ProblemDAO.java
+++ src/br/edu/popjudge/database/dao/ProblemDAO.java
@@ -8,7 +8,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.concurrent.locks.StampedLock;
 
 import javax.faces.bean.ManagedBean;
 
